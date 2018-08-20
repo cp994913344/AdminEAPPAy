88ca3f9bd566dcae52aac5bcba62b9c92e3a83ac
cuipeng <cuipeng@yiside.com>
2018-08-20 16:02:23 +0800

    商品

--- AdminEAP-web/src/main/java/com/cnpc/packmall/product/entity/Product.java
+++ AdminEAP-web/src/main/java/com/cnpc/packmall/product/entity/Product.java
@@ -13,107 +13,31 @@ import javax.persistence.Table;
  * @create 2018-08-15 19:25
  **/
 @Entity
-@Table(name="TB_PACKMAIL_PRODUCT")
-public class Product extends BaseEntity{
+@Table(name="TB_PACKMALL_PRODUCT")
+public class Product extends BaseEntity {
 
-    @Header(name="商品编码")
-    @Column(name="product_code",length = 20)
+    @Header(name = "商品编码")
+    @Column(name = "product_code", length = 20)
     private String productCode;
 
-    @Header(name="商品名称")
-    @Column(name="product_name",length = 50)
+    @Header(name = "商品名称")
+    @Column(name = "product_name", length = 50)
     private String productName;
 
-    @Header(name="banner图片id")
-    @Column(name="banner_image_ids")
-    private String bannerImageIds;
-
-    @Header(name="detail图片id")
-    @Column(name="detail_image_ids")
-    private String detailImageIds;
-
-    @Header(name="type图片id")
-    @Column(name="type_image_ids")
-    private String typeImageIds;
-
-    @Header(name="颜色")
-    @Column(name="product_color")
-    private String productColor;
-
-    @Header(name="规格")
-    @Column(name="product_type")
-    private String productType;
-
-    @Header(name="质量")
-    @Column(name="product_quality")
-    private String productQuality;
-
-    @Header(name="商品状态")
-    @Column(name = "prodcut_status")
-    private Integer prodcutStatus;
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
 
     public void setProductName(String productName) {
         this.productName = productName;
     }
 
-    public void setBannerImageIds(String bannerImageIds) {
-        this.bannerImageIds = bannerImageIds;
-    }
+    public String getProductCode() {
 
-    public void setDetailImageIds(String detailImageIds) {
-        this.detailImageIds = detailImageIds;
-    }
-
-    public void setTypeImageIds(String typeImageIds) {
-        this.typeImageIds = typeImageIds;
-    }
-
-    public void setProductColor(String productColor) {
-        this.productColor = productColor;
-    }
-
-    public void setProductType(String productType) {
-        this.productType = productType;
-    }
-
-    public void setProductQuality(String productQuality) {
-        this.productQuality = productQuality;
-    }
-
-    public void setProdcutStatus(Integer prodcutStatus) {
-        this.prodcutStatus = prodcutStatus;
+        return productCode;
     }
 
     public String getProductName() {
-
         return productName;
-    }
-
-    public String getBannerImageIds() {
-        return bannerImageIds;
-    }
-
-    public String getDetailImageIds() {
-        return detailImageIds;
-    }
-
-    public String getTypeImageIds() {
-        return typeImageIds;
-    }
-
-    public String getProductColor() {
-        return productColor;
-    }
-
-    public String getProductType() {
-        return productType;
-    }
-
-    public String getProductQuality() {
-        return productQuality;
-    }
-
-    public Integer getProdcutStatus() {
-        return prodcutStatus;
     }
 }
---
+++ AdminEAP-web/src/main/java/com/cnpc/packmall/product/entity/ProductDetail.java
@@ -0,0 +1,79 @@
+package com.cnpc.packmall.product.entity;
+
+import com.cnpc.framework.annotation.Header;
+import com.cnpc.framework.base.entity.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *  商品
+ * @author cuipeng
+ * @create 2018-08-15 19:25
+ **/
+@Entity
+@Table(name="TB_PACKMALL_PRODUCT_DETAIL")
+public class ProductDetail extends BaseEntity{
+
+    @Header(name="商品id")
+    @Column(name="product_id",length = 36)
+    private String productId;
+
+    @Header(name="详情类型")
+    @Column(name="detail_type",length = 50)
+    private String detailType;
+
+    @Header(name="详情表id")
+    @Column(name="detail_id")
+    private String detailId;
+
+    @Header(name="详情表value")
+    @Column(name="detail_val")
+    private String detailVal;
+
+    @Header(name="顺序")
+    @Column(name="detail_seq")
+    private Integer detailSeq;
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public void setDetailType(String detailType) {
+        this.detailType = detailType;
+    }
+
+    public void setDetailId(String detailId) {
+        this.detailId = detailId;
+    }
+
+    public void setDetailVal(String detailVal) {
+        this.detailVal = detailVal;
+    }
+
+    public void setDetailSeq(Integer detailSeq) {
+        this.detailSeq = detailSeq;
+    }
+
+    public String getProductId() {
+
+        return productId;
+    }
+
+    public String getDetailType() {
+        return detailType;
+    }
+
+    public String getDetailId() {
+        return detailId;
+    }
+
+    public String getDetailVal() {
+        return detailVal;
+    }
+
+    public Integer getDetailSeq() {
+        return detailSeq;
+    }
+}
